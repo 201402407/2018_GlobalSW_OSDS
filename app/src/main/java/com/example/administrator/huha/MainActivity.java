afb698609733b8c229db65f71abd656f656026d7
LeeHaeWon <banner4@cnu.ac.kr>
2018-08-17 21:25:49 +0900

    splash 및 기본 틀

---
+++ app/src/main/java/com/example/administrator/huha/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.administrator.huha;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    BroadcastReceiver receiver;
+    Intent intentMyService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 액션 바 Title 설정
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("Huha 메인 화면 액션 바");
+
+        Log.d("MpMainActivity", "service start!!!!!!");
+
+        // immortal service 등록
+        intentMyService = new Intent(this, PersistentService.class);
+        //  intentMyService.putExtra("command", "start");
+        // 리시버 등록
+        receiver = new RestartService();
+
+        try
+        {
+            // xml에서 정의해도 됨
+            // 지워도 됨
+            IntentFilter mainFilter = new IntentFilter("com.hamon.GPSservice.ssss");
+            // 리시버 저장
+            registerReceiver(receiver, mainFilter);
+            // 서비스 시작
+            Toast.makeText(this, "Start!", Toast.LENGTH_LONG).show();
+            startService(intentMyService);
+
+        } catch (Exception e) {
+
+            Log.d("MpMainActivity", e.getMessage()+"");
+            e.printStackTrace();
+        }
+
+    }
+
+    public void OnDestroy() {
+        Log.d("MpMainActivity", "Service Destroy");
+
+        // 리시버 삭제를 하지 않으면 에러
+        unregisterReceiver(receiver);
+
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/example/administrator/huha/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.administrator.huha;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+    private Handler mHandler;
+    private Runnable mRunnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // MainActivity.class 자리에 다음에 넘어갈 액티비티를 넣어주기
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("state", "launch");
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        // Handler를 통한 대기시간 설정
+        // (임의로 출력 확인을 위해 적은것 뿐, 원래는 앱 로딩 끝나면 바로 넘어가게 되어있음)
+        mHandler = new Handler();
+        // mRunnable 내부 run() 실행하려면 기다려야 하는 delayMillis
+        mHandler.postDelayed(mRunnable, 4000);
+    }
+}
